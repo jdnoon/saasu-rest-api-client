7f419525b41019fc3daa4cb712f3ed8dbb322962
Mr Terry Cullen <uqcullen@uq.edu.au>
2016-07-01 22:25:27 +1000

    Removing parent trait

--- src/Entity.php
+++ src/Entity.php
@@ -17,11 +17,52 @@ namespace Terah\Saasu;
  * @package Terah\Saasu
  */
 abstract class Entity
-{
-    use RestClientTrait;
+{å
+    /** @var RestClient */
+    protected $saasu                = null;
+    protected $filters              = [];
+    protected $pluralAttribute      = '';
+    protected $singularAttribute    = '';
 
     public function __construct(RestClient $saasu)
     {
         $this->saasu = $saasu;
     }
+
+    /**
+     * @return string
+     */
+    public function getPlural()
+    {
+        Assert($this->pluralAttribute)->notEmpty();
+        return $this->pluralAttribute;
+    }
+
+
+    /**
+     * @param null|int $id
+     * @return string
+     */
+    public function getSingular($id=null)
+    {
+        Assert($this->singularAttribute)->notEmpty();
+        $idReference = $id ? "/{$id}" : '';
+        return $this->singularAttribute . $idReference;
+    }
+
+    /**
+     * @return $this
+     */
+    public function reset()
+    {
+        $this->saasu->reset();
+        return $this;
+    }
+
+    /**
+     * @param $data
+     * @return Value
+     */
+    abstract protected function getValueObject(\stdClass $data);
+
 }
